b29e43bf2b4c18f9ba1216ab0d14b5507860786a
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-25 21:55:00 -0500

    Add else block to toMilesPerHour

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         SpeedConverter mySpeed = new SpeedConverter();
+        System.out.println(mySpeed.toMilesPerHour(10.25));
         mySpeed.printConversion(75.114);
     }
 }
--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -4,16 +4,16 @@ public class SpeedConverter {
         // Initial test
         if (kilometersPerHour < 0) {
             return -1; // indicates an invalid value
+        } else {
+            // Multiply the number of kilometers by 0.62137
+            double miles = kilometersPerHour * 0.62137;
+
+            // Round value
+            long milesRounded = Math.round(miles);
+
+            // Return as type long
+            return milesRounded;
         }
-
-        // Multiply the number of kilometers by 0.62137
-        double miles = kilometersPerHour * 0.62137;
-
-        // Round value
-        long milesRounded = Math.round(miles);
-
-        // Return as type long
-        return milesRounded;
     }
 
     public void printConversion(double kilometersPerHour) {
