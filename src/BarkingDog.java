d3c3f8d1ece3e5284fa046ed2e8e53999632d00f
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-26 11:34:07 -0500

    Create shouldWakeUp method and test

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,3 +1,15 @@
 public class BarkingDog {
-    
+    public boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        // hourOfDay should have a valid range of 0-23
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else {
+            // Wake up if dog is barking before 7 or after 22 hours
+            if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // code
+        BarkingDog dog1 = new BarkingDog();
+        System.out.println(dog1.shouldWakeUp(true, -1));
     }
 }
