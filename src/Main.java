e8fba9043dcab3b91b31422c1c1018c0373b591f
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-25 21:35:31 -0500

    Testing toMilesPerHour

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         SpeedConverter mySpeed = new SpeedConverter();
-        System.out.println(mySpeed.toMilesPerHour(10.0));
+        System.out.println(mySpeed.toMilesPerHour(75.114));
     }
 }
