52872a6680b17f91a7ffdbd1cfb5cfeafeed994a
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-26 11:37:33 -0500

    Create LeapYearCalculator class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        BarkingDog dog1 = new BarkingDog();
-        System.out.println(dog1.shouldWakeUp(true, -1));
+        // code
     }
 }
