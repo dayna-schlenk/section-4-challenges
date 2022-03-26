10436d7d7867e6893cc923829cf52b37d3666c4a
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-25 21:31:57 -0500

    Add if statement to toMilesPerHour method

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,6 +1,11 @@
 public class SpeedConverter {
     
     public long toMilesPerHour(double kilometersPerHour) {
+        // Initial test
+        if (kilometersPerHour < 0) {
+            return -1; // indicates an invalid value
+        }
+
         // Multiply the number of kilometers by 0.62137
         double miles = kilometersPerHour * 0.62137;
 
