73dce79a34178777c14ba5b2fb30091c0ed5aeb9
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-26 11:12:43 -0500

    Create BarkingDog class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MegaBytesConverter myTry = new MegaBytesConverter();
-        myTry.printMegaBytesAndKiloBytes(2500);
+        // code
     }
 }
