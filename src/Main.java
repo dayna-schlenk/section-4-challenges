9d961b0a1469a77a924ddd0fed2b24cd16675edd
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-25 22:08:33 -0500

    Create initial MegaBytesConverter class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        SpeedConverter mySpeed = new SpeedConverter();
-        System.out.println(mySpeed.toMilesPerHour(95.75));
-        mySpeed.printConversion(95.75);
+        // code
     }
 }
