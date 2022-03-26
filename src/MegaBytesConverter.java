bf071e90b9b826b7f20ed1478f50ddab19c6f7d4
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-26 10:33:17 -0500

    Create printMegaBytes method and add if statement and comments

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,12 @@
 public class MegaBytesConverter {
-    // code
+    public void printMegaBytesAndKiloBytes(int kiloBytes) {
+        // Check if invalid
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            // Calculate the megabytes and remaining kilobytes from the kilobytes parameter
+
+            // Print message in the proper format
+        }
+    }
 }
