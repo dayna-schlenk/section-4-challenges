b5209fbe8680c4343abf9a051265ea40983e9496
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-27 17:18:39 -0500

    Create Bio class

---
+++ src/Bio.java
@@ -0,0 +1,3 @@
+public class Bio {
+    // code
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.PrimitiveIterator.OfDouble;
 
 public class Main {
     public static void main(String[] args) {
-        double[] myArray = {2.3, -5, 4.25, 66};
-        SumArrays mySum = new SumArrays();
-        System.out.println(mySum.sum(myArray));
+        // code
     }
 }
