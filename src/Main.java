0bf6bf5933a9c5abafee8201ef7a40ef75b764bf
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-27 16:44:55 -0500

    Solve SumArrays challenge

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.PrimitiveIterator.OfDouble;
 
 public class Main {
     public static void main(String[] args) {
-        double[] myArray = {25.5, 63.4, 78.2};
+        double[] myArray = {};
         SumArrays mySum = new SumArrays();
         System.out.println(mySum.sum(myArray));
     }
--- src/SumArrays.java
+++ src/SumArrays.java
@@ -1,9 +1,15 @@
 public class SumArrays {
     public double sum(double[] numbers) {
         if (numbers.length == 0) {
-            return 0.0;
-        } else {
-            return 1.1;
+            return 0;
         }
+
+        double solution = 0;
+
+        for (double d : numbers) {
+            solution = solution + d;
+        }
+
+        return solution;
     }
 }
