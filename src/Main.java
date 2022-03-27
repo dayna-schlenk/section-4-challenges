a4a6fe78fb42d649fe6c2b18909171d053a3e67e
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-27 16:27:34 -0500

    Create sum method and test with if statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.PrimitiveIterator.OfDouble;
+
 public class Main {
     public static void main(String[] args) {
-        // code
+        double[] myArray = {25.5, 63.4, 78.2};
+        SumArrays mySum = new SumArrays();
+        System.out.println(mySum.sum(myArray));
     }
 }
--- src/SumArrays.java
+++ src/SumArrays.java
@@ -1,3 +1,9 @@
 public class SumArrays {
-    // code
+    public double sum(double[] numbers) {
+        if (numbers.length == 0) {
+            return 0.0;
+        } else {
+            return 1.1;
+        }
+    }
 }
