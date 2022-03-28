ee5f6c6daccd40b85bd5fb1447d4320e3a04ec63
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-28 15:03:06 -0500

    Create method for DecComp class and test

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,3 +1,11 @@
 public class DecimalComparator {
-    // code
+    public boolean areEqualByThreeDecimalPlaces(double x, double y) {
+        // Convert double to int
+        // Multiplying the ints by 1000 moves the decimals to the right by 3 places
+        int newX = (int)(x * 1000);
+        int newY = (int)(y * 1000);
+
+        // Return true if two double numbers are the same by up to 3 decimal places
+        return newX == newY;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // code
+        DecimalComparator myTry = new DecimalComparator();
+        System.out.println(myTry.areEqualByThreeDecimalPlaces(-3.123, 3.123));
     }
 }
