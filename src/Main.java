3ec20fbeda52b7c55d87ad269445e3af68b7de5b
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-28 15:31:19 -0500

    Create TeenNumberChecker class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        EqualSumChecker myTry = new EqualSumChecker();
-        System.out.println(myTry.hasEqualSum(1, -1, 0));
+        // code
     }
 }
