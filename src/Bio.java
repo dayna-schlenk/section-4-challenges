ca3c4bed1a3cd86776b49fd2b71c645119b4bc76
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-28 14:32:52 -0500

    Delete bio class

--- src/Bio.java
+++
@@ -1,3 +0,0 @@
-public class Bio {
-    // code
-}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.PrimitiveIterator.OfDouble;
-
 public class Main {
     public static void main(String[] args) {
         // code
