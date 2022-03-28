de47d800a6b07e9ea0edcf765de253f8c2790f33
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-28 15:40:14 -0500

    Create isTeen method and test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         TeenNumberChecker myTry = new TeenNumberChecker();
-        System.out.println(myTry.hasTeen(22, 23, 34));
+        System.out.println(myTry.isTeen(13));
     }
 }
